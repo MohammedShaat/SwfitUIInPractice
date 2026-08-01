323816917387837301b5d3a2b5616ec2dab692be
Mohammed Shaat <mohammedshaat.it@gmail.com>
2026-08-01 02:54:15 -0400

    Initial Commit

---
+++ SwfitUIInPractice/SwfitUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUIInPracticeApp.swift
+//  SwfitUIInPractice
+//
+//  Created by Mohammed on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
